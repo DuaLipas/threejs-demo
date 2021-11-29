720b3eb82af8a18391ade37c18bb7fffcfd7a6c1
DuaLipas <mirzamohyinsks2@gmail.com>
2021-11-29 16:50:38 +0800

    Set up THREEjs

---
+++ src/index.js
@@ -0,0 +1,108 @@
+// console.log("This works");
+// Renderer (what the user sees)
+// Scene (the data)
+// Camera(the perspective)
+// Meshes (objects in the 3d world)
+// Lights
+
+const {AxesHelper} = require("three");
+const THREE = require("three");
+
+function createRenderer() {
+  let renderer = new THREE.WebGLRenderer({antialias: true});
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor("#16161d");
+  let output = document.querySelector("#output");
+  output.appendChild(renderer.domElement);
+  return renderer;
+}
+
+function createScene() {
+  return new THREE.Scene();
+}
+
+function createCamera() {
+  let camera = new THREE.PerspectiveCamera(
+    45, // Fov
+    window.innerWidth / window.innerHeight, // Aspect ratio
+    0.1, // Near Value
+    1000 // Far Value
+  );
+  camera.position.set(-30, 40, 30);
+  camera.lookAt(0, 0, 0);
+  return camera;
+}
+
+function createCube() {
+  // Geometry - actual shape/skeleton of the object
+  let geometry = new THREE.BoxGeometry(4, 4, 4);
+  // Material - The colour/how it interacts with light
+  let material = new THREE.MeshLambertMaterial({
+    // color: "tomato",
+    color: "#66B933",
+  });
+  // Create a mesh by combining the geometry and the material
+  let mesh = new THREE.Mesh(geometry, material);
+  // Return it so it will be added to the scene
+  return mesh;
+}
+
+function createSphere() {
+  // Geometry
+  let geo = new THREE.SphereGeometry(4, 30, 30);
+  // Material
+  let mat = new THREE.MeshLambertMaterial({
+    color: "dodgerblue",
+  });
+  // Mesh
+  let mesh = new THREE.Mesh(geo, mat);
+  // Return the mesh
+  return mesh;
+}
+
+function createAxesHelper() {
+  let axesHelper = new THREE.AxesHelper(40);
+  return axesHelper;
+}
+
+function createLight() {
+  let light = new THREE.PointLight("white", 1);
+  return light;
+}
+
+function createLightHelper(light) {
+  let helper = new THREE.PointLightHelper(light);
+  return helper;
+}
+
+let renderer = createRenderer();
+let scene = createScene();
+let camera = createCamera();
+let axesHelper = createAxesHelper();
+let cube = createCube();
+let sphere = createSphere();
+let light = createLight();
+let lightHelper = createLightHelper(light);
+
+light.position.x = 10;
+light.position.y = 10;
+light.position.Z = 10;
+sphere.position.x = 20;
+
+scene.add(axesHelper);
+scene.add(cube, sphere, light, lightHelper);
+
+renderer.render(scene, camera);
+
+function animate() {
+  light.position.x += 0.1;
+  // cube.rotation.y += 0.1;
+  // cube.position.x += 0.1;
+  // cube.position.y -= 0.1;
+  // cube.position.z -= 0.1;
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+animate();
